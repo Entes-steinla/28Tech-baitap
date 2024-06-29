b6f211ac9a996fb9502146d8bbbb6f2559bd16bb
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2024-06-29 13:26:02 +0700

    first commit

---
+++ Buổi 1/Bai1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("a = ");
+    scanf("%d", &a);
+    printf("b = ");
+    scanf("%d", &b);
+    printf("\nphan nguyen cua phep chia hai so nguyen a va b la: a / b = %d\n", a / b);
+    printf("\nphan du cua phep chia hai so nguyen a va b la: a %% b = %d\n", a % b);
+    return 0;
+}
