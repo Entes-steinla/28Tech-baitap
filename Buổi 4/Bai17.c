df2bdb80fc6c3bed72ab068b0ed763712a010ea7
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2024-07-15 21:34:51 +0700

    buoi 4 fix 17

--- Buổi 4/Bai17.c
+++ Buổi 4/Bai17.c
@@ -1,17 +1,23 @@
 #include "stdio.h"
+#include <math.h>
 
 int main ()
 {
     int n;
     printf("n = ");
     scanf("%d", &n);
-    for (int i = 0; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        int num = i, tong = 0;
+        int num = i, j = i, tong = 0, count = 0; // reset tổng và đếm
+        while(j != 0)
+            {
+                count++; // số chữ số
+                j /= 10;
+            }
         while (num != 0)
         {
             int dv = num % 10;
-            tong += dv * dv * dv;
+            tong += pow(dv, count);
             num /= 10;
         }
         if (i == tong)
